63d76f3b2ca85fc403786de066e6591b8885d84a
Mukesh Kumar Patel <mukesh.patel@lenskart.in>
2021-03-07 13:03:04 +0530

    test2 commit

--- app/src/main/java/com/themovies/servicedemo/intentservice/MyIntentService.kt
+++ app/src/main/java/com/themovies/servicedemo/intentservice/MyIntentService.kt
@@ -49,6 +49,7 @@ class MyIntentService : IntentService("MyIntentService") {
 
     override fun onDestroy() {
         super.onDestroy()
+
         isRunning = false
 
         Log.d("MyIntentService", "onDestroy: ")
